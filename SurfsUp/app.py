742b240a8529dbd9ee5bfac4ff7af84741ee2537
SmitaS1 <smitashah0405@gmail.com>
2024-01-26 13:01:38 -0500

    updated the py file

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -36,8 +36,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/station<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
-        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/temp/[start_date format:mm-dd-yyyy]<br/>"
+        f"/api/v1.0/temp/[start_date format:mm-dd-yyyy]/[end_date format:mm-dd-yyyy]<br/>"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -115,58 +115,41 @@ def tobs():
     session.close()
     return jsonify(all_stations)
 
-##################################################################################################
-  
-@app.route("/api/v1.0/<start_date>")
-def startDateOnly(start_date):
-
-# Create our session (link) from Python to the DB
-    session = Session(engine)
-    Base.metadata.create_all(bind=engine)
-
-
-# Create a query for a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date.
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-       filter(Measurement.date >= start_date).all()
-    
-    start_date_tobs = []
-    for min, avg, max in results:
-        start_date_tobs_dict = {}
-        start_date_tobs_dict["min_temp"] = min
-        start_date_tobs_dict["avg_temp"] = avg
-        start_date_tobs_dict["max_temp"] = max
-        start_date_tobs.append(start_date_tobs_dict) 
-    
-    session.close()
-    return jsonify(start_date_tobs)
     
  ##################################################################################################   
-
-@app.route("/api/v1.0/<start_date>/<end_date>")
-def startDateEndDate(start_date,end_date):
-   
-# Create our session (link) from Python to the DB
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    """Return TMIN, TAVG, TMAX."""
     session = Session(engine)
     Base.metadata.create_all(bind=engine)
 
+    # Select statement
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
-# Create a query to find the minimum temperature, the average temperature, and the maximum temperature for a specified start date
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
-    
-    start_end_tobs = []
-    for min, avg, max in results:
-        start_end_tobs_dict = {}
-        start_end_tobs_dict["min_temp"] = min
-        start_end_tobs_dict["avg_temp"] = avg
-        start_end_tobs_dict["max_temp"] = max
-        start_end_tobs.append(start_end_tobs_dict) 
+    if not end:
+        start = dt.datetime.strptime(start, "%m-%d-%Y")
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
 
-    
+        session.close()
+
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    # calculate TMIN, TAVG, TMAX with start and stop
+    start = dt.datetime.strptime(start, "%m-%d-%Y")
+    end = dt.datetime.strptime(end, "%m-%d-%Y")
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+
     session.close()
 
-    return jsonify(start_end_tobs)
-
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
   
 if __name__ == '__main__':
     app.run(debug=True)
